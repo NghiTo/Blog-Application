9f31e2824fa4207acdfc196f4d6d9e0d522367b5
Nghi To <tobanghi@gmail.com>
2023-12-06 00:03:56 +0700

    [Add] post create form

---
+++ src/main/java/com/vti/blogapp/Form/PostCreateForm.java
@@ -0,0 +1,13 @@
+package com.vti.blogapp.Form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostCreateForm
+{
+    private String title;
+    private String description;
+    private String content;
+}
