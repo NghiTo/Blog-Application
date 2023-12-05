8d9d5ea3c22be3914bd2b60a36ab593297bf11ee
Nghi To <tobanghi@gmail.com>
2023-12-05 19:32:15 +0700

    [Add] Post entity

---
+++ src/main/java/com/vti/blogapp/Entity/Post.java
@@ -0,0 +1,38 @@
+package com.vti.blogapp.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "post")
+public class Post
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", length = 50, nullable = false, unique = true)
+    private String title;
+
+    @Column(name = "description", length = 150, nullable = false)
+    private String description;
+
+    @Column(name = "content", length = 150, nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
