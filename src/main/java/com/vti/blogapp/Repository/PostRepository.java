1cc55a36175528e77f28bf3205a304848db4ca94
Nghi To <tobanghi@gmail.com>
2023-12-06 13:01:18 +0700

    [Add] repository

---
+++ src/main/java/com/vti/blogapp/Repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.vti.blogapp.Repository;
+
+import com.vti.blogapp.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>
+{
+
+}
