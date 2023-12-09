3934820955d30a3cd913cb34930a69bc1a7f3e59
Nghi To <tobanghi@gmail.com>
2023-12-09 18:28:55 +0700

    [Add] find all comments api

--- src/main/java/com/vti/blogapp/Service/CommentService.java
+++ src/main/java/com/vti/blogapp/Service/CommentService.java
@@ -2,8 +2,11 @@ package com.vti.blogapp.Service;
 
 import com.vti.blogapp.Dto.CommentDto;
 import com.vti.blogapp.Form.CommentCreateForm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommentService
 {
+    Page<CommentDto> findAll(Pageable pageable);
     CommentDto create(CommentCreateForm form, Long postId);
 }
