1a9556081dadb31025b5a27f24a04c55e0fdd230
Nghi To <tobanghi@gmail.com>
2023-12-09 21:55:35 +0700

    [Add] delete comment by email api

--- src/main/java/com/vti/blogapp/Service/CommentService.java
+++ src/main/java/com/vti/blogapp/Service/CommentService.java
@@ -13,4 +13,5 @@ public interface CommentService
     CommentDto create(CommentCreateForm form, Long postId);
     CommentDto update(CommentUpdateForm form, Long id);
     void deleteById(Long id);
+    void deleteByEmail(String email);
 }
