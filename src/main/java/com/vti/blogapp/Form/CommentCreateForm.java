0d4d73adbb7a8141b051a9d054a6c51457ac80a7
Nghi To <tobanghi@gmail.com>
2023-12-09 17:31:37 +0700

    [Add] comment create form

---
+++ src/main/java/com/vti/blogapp/Form/CommentCreateForm.java
@@ -0,0 +1,13 @@
+package com.vti.blogapp.Form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentCreateForm
+{
+    private String name;
+    private String email;
+    private String body;
+}
