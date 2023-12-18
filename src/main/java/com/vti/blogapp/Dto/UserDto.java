0df30637e6a9f58ec5d194b0454b2e1b866bb9ba
Nghi To <tobanghi@gmail.com>
2023-12-18 19:25:22 +0700

    [Add] user DTO

--- src/main/java/com/vti/blogapp/Dto/UserDto.java
+++ src/main/java/com/vti/blogapp/Dto/UserDto.java
@@ -1,2 +1,18 @@
-package com.vti.blogapp.Dto;public class UserDto {
+package com.vti.blogapp.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class UserDto
+{
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
