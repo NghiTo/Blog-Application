34ba03cbbf44ca8370a683932fa25ac09c8123c1
Nghi To <tobanghi@gmail.com>
2023-12-18 19:24:56 +0700

    [Add] user create form

--- src/main/java/com/vti/blogapp/Form/UserCreateForm.java
+++ src/main/java/com/vti/blogapp/Form/UserCreateForm.java
@@ -1,2 +1,27 @@
-package com.vti.blogapp.Form;public class UserCreateForm {
+package com.vti.blogapp.Form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class UserCreateForm
+{
+    @NotBlank
+    @Length(max = 50)
+    private String name;
+
+    @NotBlank
+    @Length(max = 20)
+    private String username;
+
+    @NotBlank
+    @Length(max = 50)
+    private String email;
+
+    @NotBlank
+    @Length(max = 16, min = 8)
+    private String password;
 }
