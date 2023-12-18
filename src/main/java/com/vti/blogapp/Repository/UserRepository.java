56a7b5dfef39027a5b7ff4580a682fe8c480d8ef
Nghi To <tobanghi@gmail.com>
2023-12-18 19:23:15 +0700

    [Add] user repository

--- src/main/java/com/vti/blogapp/Repository/UserRepository.java
+++ src/main/java/com/vti/blogapp/Repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.vti.blogapp.Repository;public interface UserRepository {
+package com.vti.blogapp.Repository;
+
+import com.vti.blogapp.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+
 }
