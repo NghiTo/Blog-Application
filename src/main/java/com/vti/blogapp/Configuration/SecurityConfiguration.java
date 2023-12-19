47fb03b741d956cdfbfdc3b55a44548823b78f88
Nghi To <tobanghi@gmail.com>
2023-12-19 20:44:28 +0700

    [Add] handle 401 status

--- src/main/java/com/vti/blogapp/Configuration/SecurityConfiguration.java
+++ src/main/java/com/vti/blogapp/Configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.vti.blogapp.Configuration;
 
+import com.vti.blogapp.Exception.ErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration
 {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, ErrorHandler errorHandler) throws Exception
     {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
@@ -23,6 +24,7 @@ public class SecurityConfiguration
                         .permitAll()
                         .anyRequest()
                         .authenticated())
+                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(errorHandler))
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
